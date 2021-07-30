dc852111923fbc38840da49d4c2834644d078d7c
Henrique Elias Silva <henriqueeliassilva@gmail.com>
2021-07-30 17:04:52 -0300

    layout and thankyou page

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,13 @@ export default function Home() {
       </Head>
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
           Progressive{' '}
           <a className="text-blue-600" href="#">
           {' '}FitBall!
           </a>
         </h1>
         <p className="mt-3 text-2xl">
-          Let's Get started.{' '}
+        Cadastre-se para participar do nosso primeiro evento on-line e gratuito.{' '}
         </p>
 
         <form method="POST" action="https://pfb.activehosted.com/proc.php" id="_form_1_" class="_form _form_1 _inline-form  _dark" novalidate>
@@ -29,25 +28,25 @@ export default function Home() {
     <input type="hidden" name="v" value="2" />
     <div >
       
-      <div className="p-5">
-        <label for="fullname" >
-          Digite seu nome 
-        </label>
+      <div className="p-4">
+        {/* <label for="fullname" >
+         <p className="text-2xl pb-2">Digite seu nome </p> 
+        </label> */}
         <div>
-          <input  className="pl-2 pb-2 px-15 py-2 border-2" type="text" id="fullname" name="fullname" placeholder="Digite seu nome" />
+          <input  className="rounded pl-2 p-4 px-40 py-2 border-2" type="text" id="fullname" name="fullname" placeholder="Digite seu nome" />
         </div>
       </div>
-      <div  >
-        <label for="email" class="_form-label">
-          Digite seu email*
-        </label>
-        <div className="pb-4" >
-          <input className="pl-2 pb-2 px-15 py-2 border-2" type="text" id="email" name="email" placeholder="Digite seu email" required/>
+      <div className="pb-4">
+        {/* <label  for="email">
+         <p className="text-2xl ">Digite seu email*</p> 
+        </label> */}
+        <div className="pb-" >
+          <input className="rounded pl-2  p-4 px-40 py-2 border-2" type="text" id="email" name="email" placeholder="Digite seu email" required/>
         </div>
       </div>
       <div >
-        <button className="pb-2 px-10 py-2 bg-green-600 text-white"  id="_form_1_submit"  type="submit" >
-          Submit
+        <button className=" uppercase rounded  px-24 py-2 bg-blue-600 text-white"  id="_form_1_submit"  type="submit" >
+          Quero participar
         </button>
       </div>
       <div >
---
+++ pages/obrigado.js
@@ -0,0 +1,20 @@
+
+
+function ThankYou () {
+    return <div className="flex flex-col items-center justify-center min-h-screen py-2">
+         <h1 className="text-6xl font-bold">
+          Obrigado!{' '}
+        </h1>
+        <p className=" text-center m-3 text-2xl">
+        Seu cadastro foi realizado com sucesso. {' '}Em breve mandaremos mais informações no seu e-mail.{' '}
+        </p>
+        <p className="text-center m-3 text-xl">Participe do nosso grupo de WhatsApp e seja {' '}um dos primeiros a saber sobre as nosso curso on-line. </p>
+        <button className="mt-5  uppercase rounded  px-24 py-2 bg-blue-600 text-white" >
+        Quero participar
+      </button>
+        
+        </div>
+        
+  }
+  
+  export default ThankYou
